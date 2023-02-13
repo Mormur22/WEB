ee078ce62e0efd375b84a1450b7d880cd07b9d3c
Morwar22 <78794067+Morwar22@users.noreply.github.com>
2023-02-13 01:04:31 +0100

    eliminar ejemplos

--- NODEJS/entradaTeclado.js
+++
@@ -1,7 +0,0 @@
-console.log("Escribe tu nombre");
-var stdin = process.openStdin();
-
-stdin.addListener("data", function(d) {
-    console.log("Tu nombre es: " + 
-        d.toString().trim());
-  });
